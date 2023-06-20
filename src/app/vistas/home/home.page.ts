d08d224970b7cc8439804f2c7b5c5fdc2b3f99cb
Néstor Pérez <njperezlopez@gmail.com>
2023-06-20 00:28:19 -0300

    Fix PushNotifications no actualizaba el token Previene el mensaje de error al ejecutar el plugin en la plataforma web

--- src/app/vistas/home/home.page.ts
+++ src/app/vistas/home/home.page.ts
@@ -1,77 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  ActionPerformed,
-  PushNotificationSchema,
-  PushNotifications,
-  Token,
-} from '@capacitor/push-notifications';
+import { ActionPerformed, PushNotificationSchema, PushNotifications, Token } from '@capacitor/push-notifications';
 import { IonicModule } from '@ionic/angular';
 import { SidebarComponent } from '../../componentes/sidebar/sidebar.component';
 import { DtTokenUser } from '../../modelos/dataTypes/DtUsuario';
 import { ApiService } from '../../servicios/api.service';
-import { DataService } from '../../servicios/data.service';
 import { JwtService } from '../../servicios/jwt.service';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.page.html',
-    styleUrls: ['./home.page.scss'],
-    standalone: true,
-    imports: [IonicModule, SidebarComponent],
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, SidebarComponent],
 })
 export class HomePage implements OnInit {
-
-  constructor(private api: ApiService, private jwtService: JwtService, private dataService: DataService) { }
-
+  constructor(private api: ApiService, private jwtService: JwtService) {}
 
   ngOnInit() {
-    this.inicializarNotificacionesFirebase();
+    if (Capacitor.isPluginAvailable('PushNotifications'))
+      this.inicializarNotificacionesFirebase();
   }
-  
+
   /* =========================== NOTIFICACIONES FIREBASE =========================== */
 
   async inicializarNotificacionesFirebase() {
-    PushNotifications.addListener('registration',
-      (token: Token) => {
-        console.log('[FCM] Registration success. Token: ' + token.value);
-        this.dataService.setData("mobileTokenFCM", token.value)
-      }
-    );
+    PushNotifications.addListener('registration', (token: Token) => {
+      console.log('[FCM] Registration success. Token: ' + token.value);
+      this.establecerUsuarioTokenFCM(token.value);
+    });
 
-    PushNotifications.addListener('registrationError',
-      (error: any) => {
-        console.log('[FCM] Error on registration: ' + JSON.stringify(error));
-      }
-    );
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('[FCM] Error on registration: ' + JSON.stringify(error));
+    });
 
     // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed',
-      (notification: ActionPerformed) => {
-        console.log('[FCM] pushNotificationActionPerformed: ' + JSON.stringify(notification));
-      }
-    );
+    PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
+      console.log('[FCM] pushNotificationActionPerformed: ' + JSON.stringify(notification));
+    });
 
     // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived',
-      (notification: PushNotificationSchema) => {
-        console.log('[FCM] Push received: ' + JSON.stringify(notification));
-      }
-    );
+    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+      console.log('[FCM] Push received: ' + JSON.stringify(notification));
+    });
 
-    PushNotifications.checkPermissions().then(result => {
+    PushNotifications.checkPermissions().then((result) => {
       if (result.receive === 'granted') {
         console.log('[FCM] checkPermissions() granted');
-        this.enviarUsuarioToken();
-      }
-      else {
+        PushNotifications.register();
+      } else {
         console.log('[FCM] checkPermissions() not granted');
 
-        PushNotifications.requestPermissions().then(result2 => {
+        PushNotifications.requestPermissions().then((result2) => {
           if (result2.receive === 'granted') {
             console.log('[FCM] requestPermissions() granted');
-            this.enviarUsuarioToken();
-          }
-          else {
+            PushNotifications.register();
+          } else {
             console.log('[FCM] requestPermissions() not granted');
           }
         });
@@ -79,25 +63,18 @@ export class HomePage implements OnInit {
     });
   }
 
-  private async enviarUsuarioToken() {
-    const mobileToken = await this.dataService.getData("mobileTokenFCM")
-
-    if (mobileToken)
-      this.establecerUsuarioTokenFCM(mobileToken);
-  }
-
   private async establecerUsuarioTokenFCM(tokenFCM: string) {
     const idUsuario = await this.jwtService.obtenerUsuarioId();
 
     const data: DtTokenUser = {
       token: tokenFCM,
-      tipo: "FCM_REGISTRATION"
-    }
+      tipo: 'FCM_REGISTRATION',
+    };
 
     this.api.establecerUsuarioTokenFCM(data, idUsuario).subscribe({
-      error: (e) => console.error(e)
+      error: (e) => console.error(e),
     });
   }
-  
+
   /* =========================== NOTIFICACIONES FIREBASE =========================== */
 }
